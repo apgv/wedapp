e2783aaf1b0b9d01f544103ef8fd3cdab5a4080e
apgv <ap@foobar.codes>
2018-01-26 23:29:58 +0100

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ import org.jooq.util.jaxb.*
 import org.jooq.util.jaxb.Target
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version ("1.2.10")
-    id("com.github.johnrengelman.shadow") version ("2.0.1")
-    id("org.flywaydb.flyway") version ("4.2.0")
+    id("org.jetbrains.kotlin.jvm") version ("1.2.21")
+    id("com.github.johnrengelman.shadow") version ("2.0.2")
+    id("org.flywaydb.flyway") version ("5.0.6")
 }
 
 buildscript {
@@ -18,10 +18,10 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.postgresql:postgresql:42.1.4")
-        classpath("org.jooq:jooq:3.10.1")
-        classpath("org.jooq:jooq-meta:3.10.1")
-        classpath("org.jooq:jooq-codegen:3.10.1")
+        classpath("org.postgresql:postgresql:42.2.1")
+        classpath("org.jooq:jooq:3.10.4")
+        classpath("org.jooq:jooq-meta:3.10.4")
+        classpath("org.jooq:jooq-codegen:3.10.4")
     }
 }
 
@@ -93,21 +93,21 @@ repositories {
 
 dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compile("com.sparkjava:spark-core:2.6.0")
+    compile("com.sparkjava:spark-core:2.7.1")
     compile("org.slf4j:slf4j-simple:1.7.25")
     compile("org.apache.logging.log4j:log4j-api:2.10.0")
     compile("org.apache.logging.log4j:log4j-core:2.10.0")
     compile("org.apache.logging.log4j:log4j-slf4j-impl:2.10.0")
     compile("com.squareup.moshi:moshi:1.5.0")
     compile("com.google.code.gson:gson:2.8.2")
-    compile("com.google.guava:guava:23.5-jre")
+    compile("com.google.guava:guava:23.6-jre")
     compile("com.auth0:java-jwt:3.3.0")
-    compile("com.auth0:jwks-rsa:0.2.0")
-    compile("com.zaxxer:HikariCP:2.7.2")
-    compile("org.jooq:jooq:3.10.1")
-    compile("org.flywaydb:flyway-core:4.2.0")
-    compile("org.postgresql:postgresql:42.1.4")
-    testCompile("org.junit.jupiter:junit-jupiter-api:5.0.1")
-    testCompile("org.assertj:assertj-core:3.8.0")
+    compile("com.auth0:jwks-rsa:0.3.0")
+    compile("com.zaxxer:HikariCP:2.7.6")
+    compile("org.jooq:jooq:3.10.4")
+    compile("org.flywaydb:flyway-core:5.0.6")
+    compile("org.postgresql:postgresql:42.2.1")
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.0.3")
+    testCompile("org.assertj:assertj-core:3.9.0")
     testCompile("org.mockito:mockito-core:2.+")
 }
