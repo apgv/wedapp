7f1672fb6cbef45e612c69d20767ebdb71d2f33f
apgv <ap@foobar.codes>
2018-01-27 00:03:34 +0100

    Changed to get server port from environment variable as required by Heroku

--- src/main/kotlin/codes/foobar/wedapp/Main.kt
+++ src/main/kotlin/codes/foobar/wedapp/Main.kt
@@ -2,8 +2,11 @@ package codes.foobar.wedapp
 
 import com.google.gson.Gson
 import spark.Spark.get
+import spark.Spark.port
 
 fun main(args: Array<String>) {
+    port(System.getenv("PORT").toInt())
+
     get("/", { _, _ ->
         Gson().toJson("hello")
     })
