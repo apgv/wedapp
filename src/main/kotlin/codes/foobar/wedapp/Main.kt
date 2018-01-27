3d5cec3cd9292bbb88d0b08aa9e5cf7890832918
apgv <ap@foobar.codes>
2018-01-27 01:27:34 +0100

    Added folder for static files to be served by Spark

--- src/main/kotlin/codes/foobar/wedapp/Main.kt
+++ src/main/kotlin/codes/foobar/wedapp/Main.kt
@@ -1,11 +1,11 @@
 package codes.foobar.wedapp
 
 import com.google.gson.Gson
-import spark.Spark.get
-import spark.Spark.port
+import spark.Spark.*
 
 fun main(args: Array<String>) {
     port(System.getenv("PORT").toInt())
+    staticFiles.location("/frontend")
 
     get("/", { _, _ ->
         Gson().toJson("hello")
