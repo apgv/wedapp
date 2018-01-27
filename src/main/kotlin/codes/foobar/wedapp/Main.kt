9ca1ed7d3af5c049067feec1d3e45c3e75a49fd2
apgv <ap@foobar.codes>
2018-01-27 22:24:47 +0100

    Force the client to connect using HTTPS on Heroku

--- src/main/kotlin/codes/foobar/wedapp/Main.kt
+++ src/main/kotlin/codes/foobar/wedapp/Main.kt
@@ -7,6 +7,22 @@ fun main(args: Array<String>) {
     port(System.getenv("PORT").toInt())
     staticFiles.location("/frontend")
 
+    before("/*", { request, response ->
+        val herokuOriginatingProtocol = request.headers("X-Forwarded-Proto")
+        val secureProtocol = herokuOriginatingProtocol?.equals("https") ?: true
+
+        if (!secureProtocol) {
+            val url = request.url()
+            val secureUrl = url.replaceRange(0, 4, "https")
+            val queryString = request.queryString()
+
+            when {
+                queryString.isNullOrBlank() -> response.redirect(secureUrl)
+                else -> response.redirect(secureUrl + "?" + queryString)
+            }
+        }
+    })
+
     get("/", { _, _ ->
         Gson().toJson("hello")
     })
