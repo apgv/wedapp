d881ec2df20dabbdf0b1108d3ca5ca0228cae0e6
apgv <ap@foobar.codes>
2018-01-29 16:03:40 +0100

    Added Moshi ZonedDateTimeAdapter

--- src/main/kotlin/codes/foobar/wedapp/helper/JsonHelper.kt
+++ src/main/kotlin/codes/foobar/wedapp/helper/JsonHelper.kt
@@ -1,7 +1,10 @@
 package codes.foobar.wedapp.helper
 
+import codes.foobar.wedapp.moshi.ZonedDateTimeAdapter
 import com.squareup.moshi.Moshi
 
 object JsonHelper {
-    val moshi: Moshi = Moshi.Builder().build()
+    val moshi: Moshi = Moshi.Builder()
+            .add(ZonedDateTimeAdapter())
+            .build()
 }
---
+++ src/main/kotlin/codes/foobar/wedapp/moshi/ZonedDateTimeAdapter.kt
@@ -0,0 +1,13 @@
+package codes.foobar.wedapp.moshi
+
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import java.time.ZonedDateTime
+
+class ZonedDateTimeAdapter {
+    @ToJson
+    fun toJson(zonedDateTime: ZonedDateTime) = zonedDateTime.toInstant().toEpochMilli()
+
+    @FromJson
+    fun fromJson(date: String) = ZonedDateTime.parse(date)
+}
