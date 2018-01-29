4d3f6550b09d1f0633b0da802dbaff1c173a4348
apgv <ap@foobar.codes>
2018-01-29 17:16:40 +0100

    Changed ZonedDateTimeAdapter to convert from milliseconds

--- src/main/kotlin/codes/foobar/wedapp/moshi/ZonedDateTimeAdapter.kt
+++ src/main/kotlin/codes/foobar/wedapp/moshi/ZonedDateTimeAdapter.kt
@@ -2,6 +2,8 @@ package codes.foobar.wedapp.moshi
 
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
+import java.time.Instant
+import java.time.ZoneId
 import java.time.ZonedDateTime
 
 class ZonedDateTimeAdapter {
@@ -9,5 +11,5 @@ class ZonedDateTimeAdapter {
     fun toJson(zonedDateTime: ZonedDateTime) = zonedDateTime.toInstant().toEpochMilli()
 
     @FromJson
-    fun fromJson(date: String) = ZonedDateTime.parse(date)
+    fun fromJson(date: String) = ZonedDateTime.ofInstant(Instant.ofEpochMilli(date.toLong()), ZoneId.of("Europe/Oslo"))
 }
